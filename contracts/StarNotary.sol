bfcdc3975f97080779a43d1121eb4ffb16462c6a
Steve Redka <cogitofallacy@gmail.com>
2019-08-28 11:36:31 +0300

    task 1: StarNotary functions

--- contracts/StarNotary.sol
+++ contracts/StarNotary.sol
@@ -56,23 +56,34 @@ contract StarNotary is ERC721 {
         }
     }
 
-    // Implement Task 1 lookUptokenIdToStarInfo
     function lookUptokenIdToStarInfo (uint _tokenId) public view returns (string memory) {
-        //1. You should return the Star saved in tokenIdToStarInfo mapping
+        return tokenIdToStarInfo[_tokenId].name;
     }
 
     // Implement Task 1 Exchange Stars function
     function exchangeStars(uint256 _tokenId1, uint256 _tokenId2) public {
         //1. Passing to star tokenId you will need to check if the owner of _tokenId1 or _tokenId2 is the sender
+        address sender = msg.sender;
         //2. You don't have to check for the price of the token (star)
-        //3. Get the owner of the two tokens (ownerOf(_tokenId1), ownerOf(_tokenId1)
+        // ok
+        //3. Get the owners of the two tokens (ownerOf(_tokenId1), ownerOf(_tokenId2)
+        address user1 = ownerOf(_tokenId1);
+        address user2 = ownerOf(_tokenId2);
         //4. Use _transferFrom function to exchange the tokens.
+        if (user1 == sender || user2 == sender) {
+          _transferFrom(user1, user2, _tokenId1);
+          _transferFrom(user2, user1, _tokenId2);
+        }
     }
 
     // Implement Task 1 Transfer Stars
     function transferStar(address _to1, uint256 _tokenId) public {
         //1. Check if the sender is the ownerOf(_tokenId)
-        //2. Use the transferFrom(from, to, tokenId); function to transfer the Star
+        address address1 = ownerOf(_tokenId);
+        if (msg.sender == address1) {  
+            //2. Use the transferFrom(from, to, tokenId); function to transfer the Star
+            _transferFrom(address1, _to1, _tokenId);
+        }
     }
 
 }
